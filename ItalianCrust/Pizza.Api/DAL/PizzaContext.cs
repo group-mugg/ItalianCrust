a85ca1a659c1d6e9b4ba8c35fe3e4b9ca297eea1
jova93 <89088512+jova93@users.noreply.github.com>
2022-12-18 14:19:16 +0100

    rm comment

--- ItalianCrust/Pizza.Api/DAL/PizzaContext.cs
+++ ItalianCrust/Pizza.Api/DAL/PizzaContext.cs
@@ -9,13 +9,5 @@ public class PizzaContext : DbContext
     {
     }
 
-    //protected override void OnModelCreating(ModelBuilder modelBuilder)
-    //{
-    //    modelBuilder.Entity<Models.Pizza>()
-    //        .HasData(
-    //            new Models.Pizza { Id = 1, Name = "P1", Description = "D1", Price = 1.11M }
-    //        );
-    //}
-
     public DbSet<Models.Pizza> Pizzas { get; set; }
 }
