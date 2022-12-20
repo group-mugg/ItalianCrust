fabd931c3d5cdd7795f200b0771ec0f80c0cacb5
jova93 <89088512+jova93@users.noreply.github.com>
2022-12-20 13:28:29 +0100

    ok

--- ItalianCrust/Order.UnitTests/MoqTests/GetAllOrdersHandlerTests.cs
+++ ItalianCrust/Order.UnitTests/MoqTests/GetAllOrdersHandlerTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Moq;
+using Order.Api.DTOs;
+using Order.Api.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +9,18 @@ using System.Threading.Tasks;
 
 namespace Order.UnitTests.MoqTests;
 
-internal class GetAllOrdersHandlerTests
+public class GetAllOrdersHandlerTests
 {
+    [Fact]
+    public async Task HandleAsync_WhenThereAreOrdersInDatabase_ReturnsOk()
+    {
+        //Arrange
+        var mock = new Mock<IOrderRepository>();
+
+        mock.Setup(m => m.GetAllOrders())
+            .ReturnsAsync(new List<OrderDTO>
+            {
+                new OrderDTO { Id = 1, CustomerName = "Test"}
+            });
+    }
 }
--- ItalianCrust/Order.UnitTests/MoqTests/GetOrderByIdHandlerTests.cs
+++ ItalianCrust/Order.UnitTests/MoqTests/GetOrderByIdHandlerTests.cs
@@ -1,11 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Moq;
+using Order.Api.DTOs;
+using Order.Api.Handlers;
+using Order.Api.Repositories;
 
 namespace Order.UnitTests.MoqTests;
 
-internal class GetOrderByIdHandlerTests
+public class GetOrderByIdHandlerTests
 {
+    [Fact]
+    public async Task HandleAsync_WhenOrderDoesNotExist_ReturnsNotFound()
+    {
+        //Arrange
+        var mock = new Mock<IOrderRepository>();
+
+        mock.Setup(m => m.GetOrderById(It.Is<int>(id => id == 1)))
+            .ReturnsAsync((OrderDTO?)null);
+
+        //Act
+        var notFoundResult = (NotFound)await GetOrderByIdHandler.HandleAsync(mock.Object, 1);
+
+        //Assert
+        Assert.Equal(404, notFoundResult.StatusCode);
+    }
+
+    [Fact]
+    public async Task HandleAsync_WhenOrderExists_ReturnsOk()
+    {
+        //Arrange
+        var mock = new Mock<IOrderRepository>();
+
+        mock.Setup(m => m.GetOrderById(It.Is<int>(id => id == 1)))
+            .ReturnsAsync(new OrderDTO
+            {
+                Id = 1,
+                CustomerName = "Test",
+                TotalOrder = 1.00M,
+                OrderDate = DateTime.UtcNow
+            });
+
+        //Act
+        var okResult = (Ok<OrderDTO>)await GetOrderByIdHandler.HandleAsync(mock.Object, 1);
+
+        //Assert
+        Assert.Equal(200, okResult.StatusCode);
+        var foundOrder = Assert.IsAssignableFrom<OrderDTO>(okResult.Value);
+        Assert.Equal(1, foundOrder.Id);
+    }
 }
