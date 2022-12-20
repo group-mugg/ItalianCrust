f65341625766f65511e001a3a89775e3022b04aa
jova93 <89088512+jova93@users.noreply.github.com>
2022-12-20 11:43:43 +0100

    additional fix

--- ItalianCrust/Order.Api/Extensions/WebApplicationExtensions.cs
+++ ItalianCrust/Order.Api/Extensions/WebApplicationExtensions.cs
@@ -11,12 +11,19 @@ public static class WebApplicationExtensions
 
         //Create
         app.MapPost(CreateOrderEndpoint.Pattern, CreateOrderEndpoint.Handler);
+        app.MapPost(CreatePizzaEndpoint.Pattern, CreatePizzaEndpoint.Handler);
 
         //Read
         app.MapGet(GetAllOrdersEndpoint.Pattern, GetAllOrdersEndpoint.Handler);
         app.MapGet(GetOrderByIdEndpoint.Pattern, GetOrderByIdEndpoint.Handler);
+        app.MapGet(GetAllPizzasEndpoint.Pattern, GetAllPizzasEndpoint.Handler);
+        app.MapGet(GetPizzaByIdEndpoint.Pattern, GetPizzaByIdEndpoint.Handler);
+
+        //Update
+        app.MapPut(EditPizzaEndpoint.Pattern, EditPizzaEndpoint.Handler);
 
         //Delete
         app.MapDelete(DeleteOrderEndpoint.Pattern, DeleteOrderEndpoint.Handler);
+        app.MapDelete(DeletePizzaEndpoint.Pattern, DeletePizzaEndpoint.Handler);
     }
 }
--- ItalianCrust/Order.Api/Program.cs
+++ ItalianCrust/Order.Api/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Add services below.
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IPizzaRepository, PizzaRepository>();
 
 var app = builder.Build();
 
