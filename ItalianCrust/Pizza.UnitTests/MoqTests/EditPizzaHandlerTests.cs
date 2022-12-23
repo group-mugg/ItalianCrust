16dde685e537da195056b45eccbb37f792fa9ad5
b13gusbe <gustav.bergstedt92@yahoo.se>
2022-12-23 15:02:58 +0100

    Removed outcommented code

--- ItalianCrust/Pizza.UnitTests/MoqTests/EditPizzaHandlerTests.cs
+++ ItalianCrust/Pizza.UnitTests/MoqTests/EditPizzaHandlerTests.cs
@@ -50,42 +50,4 @@ public class EditPizzaHandlerTests
         var response = Assert.IsAssignableFrom<bool>(okResult.Value);
         Assert.True(response);
     }
-
-
-    //[Fact]
-    //public async Task HandleAsync_WhenPizzaDoesNotExist_ReturnsNotFound()
-    //{
-    //    //Arrange
-    //    var mock = new Mock<IPizzaRepository>();
-
-    //    mock.Setup(m => m.GetPizzaById(It.Is<int>(id => id == 1)))
-    //        .ReturnsAsync((PizzaDTO?)null);
-
-    //    //Act
-    //    var notFoundResult = (NotFound)await GetPizzaByIdHandler.HandleAsync(mock.Object, 1);
-
-    //    //Assert
-    //    Assert.Equal(404, notFoundResult.StatusCode);
-    //}
-
-    //[Fact]
-    //public async Task HandleAsync_WhenPizzaExists_EditsPizza()
-    //{
-    //    //Arrange
-    //    var mock = new Mock<IPizzaRepository>();
-
-    //    mock.Setup(m => m.GetPizzaById(It.Is<int>(id => id == 1)))
-    //        .ReturnsAsync(
-    //            new PizzaDTO { Id = 1, Name = "Mock", Description = "Mock", Price = 1.00m }
-    //        );
-
-    //    //Act
-    //    var okResult = (Ok<PizzaDTO>)await GetPizzaByIdHandler.HandleAsync(mock.Object, 1);
-
-    //    //Assert
-    //    Assert.Equal(200, okResult.StatusCode);
-    //    var returnedPizza = Assert.IsAssignableFrom<PizzaDTO>(okResult.Value);
-    //    returnedPizza.Name = "Edited mock";
-    //    Assert.Equal("Edited mock", returnedPizza.Name);
-    //}
 }
